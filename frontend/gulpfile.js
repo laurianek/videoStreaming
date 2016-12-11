df76a838267b29d54c44e9db39a6ec645c33795d
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2016-12-11 21:52:03 +0000

    video on screen

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,8 +2,16 @@
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 const rev = require('./dist/rev.json');
+const less = require('gulp-less');
 
-gulp.task('post-build', () => {
+gulp.task('styles', () => {
+  return gulp.src('src/styles.less')
+    .pipe(less())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('build', ['styles'], () => {
+
   var bundle = rev['main.js'];
   return gulp.src('src/index.html')
     .pipe(replace('main.js', bundle))
